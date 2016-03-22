94c7427eb15cb8c41e66c6a93c18c62175351e4d
Islam El-Nabarawy <islam.ossama@gmail.com>
2016-03-22 17:25:04 -0500

    Refactor ART class test functions

--- tests/TestOnlineFuzzyART.py
+++ tests/TestOnlineFuzzyART.py
@@ -14,7 +14,7 @@ class TestOnlineFuzzyART(unittest.TestCase):
         fuzzyart.run_batch(np.zeros((0, 0)), 100)
         self.assertEqual(fuzzyart.num_clusters, 0)
 
-    def test_ml1m_user_data(self):
+    def test_online_mode(self):
         with XCSVFileReader('data/users.csv') as reader:
             fa = OnlineFuzzyART(0.95, 0.001, 0.9, reader.num_fields)
             data_ranges = [(0, 1), (0, 6)] + [(0, 1)] * 21
@@ -23,18 +23,13 @@ class TestOnlineFuzzyART(unittest.TestCase):
         self.assertEqual(52, fa.num_clusters)
 
     def test_batch_mode(self):
-        data_ranges = [(0, 1), (0, 6)] + [(0, 1)] * 21
-
-        def normalize(p):
-            for i in range(len(p)):
-                p[i] = utils.scale_range(p[i], data_ranges[i])
-            return p
-
         with XCSVFileReader('data/users.csv') as reader:
             dataset = np.zeros((len(reader), reader.num_fields))
+            data_ranges = [(0, 1), (0, 6)] + [(0, 1)] * 21
             for ix, row in enumerate(reader):
                 pattern = np.array(row, dtype=float)
-                normalize(pattern)
+                for i in range(len(pattern)):
+                    pattern[i] = utils.scale_range(pattern[i], data_ranges[i])
                 dataset[ix, :] = pattern
         fa = OnlineFuzzyART(0.95, 0.001, 0.9, reader.num_fields)
         iterations, clusters = fa.run_batch(dataset, max_epochs=100, seed=100)
