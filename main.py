0efa54003cd279f3929f360fb2ab57ca21cd3c10
Islam El-Nabarawy <islam.ossama@gmail.com>
2016-04-21 10:00:51 -0500

    Make some refactoring and adjustments to main code

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ from numpy import linalg as la
 
 from sklearn.metrics import adjusted_rand_score
 
-import utils
+from data import read_dataset
 from ART import OnlineFuzzyART
-from data import XCSVFileReader
 
 __author__ = 'Islam Elnabarawy'
 
 # FuzzyART algorithm parameters
-rho, alpha, beta = 0.555584128569494, 0.7568376651388451, 0.5837066700986062
+rho, alpha, beta = 0.5342327836238868, 0.3119799068437582, 0.5472496744945247   # iris.data
+# rho, alpha, beta = 0.4249555132101839, 0.0011891228422072908, 0.5315274236032594   # glass.data
+
+data_file = 'data/iris.data'
+# data_file = 'data/glass.data'
 
 
 def max_norm(x):
@@ -30,7 +33,7 @@ def fuzzy_and(x, y):
     return np.min(np.array([x, y]), 0)
 
 
-def protectedDiv(left, right):
+def protected_div(left, right):
     try:
         return left / right
     except ZeroDivisionError:
@@ -41,7 +44,7 @@ pset = gp.PrimitiveSetTyped("main", [np.ndarray, np.ndarray, float], float)
 pset.addPrimitive(operator.add, [float, float], float)
 pset.addPrimitive(operator.sub, [float, float], float)
 pset.addPrimitive(operator.mul, [float, float], float)
-pset.addPrimitive(protectedDiv, [float, float], float)
+pset.addPrimitive(protected_div, [float, float], float)
 pset.addPrimitive(operator.neg, [float], float)
 pset.addPrimitive(max_norm, [np.ndarray], float)
 pset.addPrimitive(fuzzy_and, [np.ndarray, np.ndarray], np.ndarray)
@@ -50,8 +53,8 @@ pset.renameArguments(ARG0="A")
 pset.renameArguments(ARG1="w_j")
 pset.renameArguments(ARG2="alpha")
 
-creator.create("FitnessMin", base.Fitness, weights=(1.0,))
-creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=5)
@@ -59,27 +62,22 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.ex
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-with XCSVFileReader('data/iris.data') as reader:
-    dataset = np.zeros((len(reader), reader.num_fields-1))
-    labels = np.zeros((len(reader)))
-    data_ranges = [(4.3, 7.9), (2.0, 4.4), (1.0, 6.9), (0.1, 2.5)]
-    for ix, row in enumerate(reader):
-        pattern = np.array(row[:4], dtype=float)
-        for i in range(len(pattern)):
-            pattern[i] = utils.scale_range(pattern[i], data_ranges[i])
-        dataset[ix, :] = pattern
-        labels[ix] = row[4]
+dataset, labels = read_dataset(data_file)
 
 
 def eval_individual(individual):
     # Transform the tree expression in a callable function
     func = toolbox.compile(expr=individual)
+    # print("Evaluating individual: %s" % individual)
     # Create a FuzzyART network with the individual's category choice function
-    fa = OnlineFuzzyART(rho, alpha, beta, len(data_ranges), choice_fn=func)
+    fa = OnlineFuzzyART(rho, alpha, beta, dataset.shape[1], choice_fn=func)
     # Run the clustering on the dataset and find the clusters
-    iterations, clusters = fa.run_batch(dataset, max_epochs=100, seed=100)
+    iterations, clusters = fa.run_batch(dataset, max_epochs=10, seed=100)
     # return the adjusted rand index for the result
-    return adjusted_rand_score(labels, clusters),
+    performance = adjusted_rand_score(labels, clusters)
+    # log the result
+    # print("\tIterations: %02d\tPerformance: %s" % (iterations, performance))
+    return performance,
 
 toolbox.register("evaluate", eval_individual)
 toolbox.register("select", tools.selTournament, tournsize=3)
@@ -98,6 +96,10 @@ mstats.register("std", np.std)
 mstats.register("min", np.min)
 mstats.register("max", np.max)
 
-pop = toolbox.population(n=30)
-hof = tools.HallOfFame(5)
-pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 40, stats=mstats, halloffame=hof, verbose=True)
+pop = toolbox.population(n=50)
+hof = tools.HallOfFame(3)
+pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.3, 40, stats=mstats, halloffame=hof, verbose=True)
+
+print("Hall of fame:")
+for expr in hof:
+    print('\t', expr, expr.fitness)
