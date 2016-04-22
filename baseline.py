46fa67968983fb3bd8b71aebb5c813237604a16d
Islam El-Nabarawy <islam.ossama@gmail.com>
2016-04-21 20:23:59 -0500

    Average the baseline results over 100 different evaluations

--- baseline.py
+++ baseline.py
@@ -1,3 +1,6 @@
+import multiprocessing
+
+import numpy as np
 from sklearn.metrics import adjusted_rand_score
 
 from data import read_dataset
@@ -5,16 +8,32 @@ from ART import OnlineFuzzyART
 
 __author__ = 'Islam Elnabarawy'
 
-rho, alpha, beta = 0.5342327836238868, 0.3119799068437582, 0.5472496744945247   # iris.data
+rho, alpha, beta = 0.5173929115731474, 0.47460905154087896, 0.6250151337909732   # iris.data
 # rho, alpha, beta = 0.4249555132101839, 0.0011891228422072908, 0.5315274236032594     # glass.data
 
 data_file = 'data/iris.data'
 # data_file = 'data/glass.data'
 
 dataset, labels = read_dataset(data_file)
-fa = OnlineFuzzyART(rho, alpha, beta, dataset.shape[1])
-iterations, clusters = fa.run_batch(dataset, max_epochs=100, seed=100)
-performance = adjusted_rand_score(labels, clusters)
 
-print('FuzzyART found %s clusters.' % fa.num_clusters)
-print('Adjusted Rand Index: %s' % performance)
+
+def evaluate(index):
+    fa = OnlineFuzzyART(rho, alpha, beta, dataset.shape[1])
+    iterations, clusters = fa.run_batch(dataset, max_epochs=100)
+    performance = adjusted_rand_score(labels, clusters)
+    return index, iterations, fa.num_clusters, performance
+
+
+def main():
+    pool = multiprocessing.Pool()
+    results = pool.map(evaluate, range(100))
+    print('%-4s\t%-4s\t%-8s\t%s' % ('Idx', 'Iter', 'Clusters', 'Performance'))
+    print('-'*50)
+    for row in results:
+        print('%-4s\t%-4s\t%-8s\t%s' % row)
+    print('-'*50)
+    avg = np.array(results, dtype=float)[:, 1:].mean(axis=0)
+    print('Avg:\t%-4s\t%-8s\t%s' % tuple(avg))
+
+if __name__ == '__main__':
+    main()
